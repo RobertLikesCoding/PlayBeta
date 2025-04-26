b724c4d27252a12fd144f3887a2589c31c77f5d8
Robert Baufeld <rbaufeld@posteo.de>
2025-04-26 20:43:24 +0200

    install new frontend

---
+++ frontend/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Wow, is this really it?
+  </div>
+  <UButton color="secondary">TESTER</UButton>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped></style>
