f92e579fd246d5823340ae38ef44514f2ddf6fe0
Robert Baufeld <rbaufeld@posteo.de>
2025-04-28 21:53:36 +0200

    create folder structure and pages

---
+++ frontend/pages/auth/signup.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Sign Up</h1>
+  <form action=""></form>
+</template>
+
+
+<script setup>
+  definePageMeta({
+    layout: 'auth'
+  })
+
+  const form = useForm({
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value)
+    },
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  })
+
+</script>
