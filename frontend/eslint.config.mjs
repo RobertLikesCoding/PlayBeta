9d9c5eb38bfc942979609daa100ba62b98ac0f56
Robert Baufeld <rbaufeld@posteo.de>
2025-05-05 22:26:39 +0200

    install and setup eslint and prettier

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,10 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-config-prettier'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  extends: [prettier],
+  rules: {
+    'prettier/prettier': 'warn', // shows Prettier issues in ESLint
+    // your other rules here...
+  },
+})
