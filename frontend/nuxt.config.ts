4ec65c7006e8f7bf5a4a0beb0a783e558ce20b87
Robert Baufeld <rbaufeld@posteo.de>
2025-05-13 22:33:55 +0200

    configuration for backend communication

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,17 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   devServer: { port: 3001 },
 
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/test-utils",
-    "@nuxt/ui",
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
   ],
 
-  css: ["@/assets/css/main.css"],
-});
+  css: ['@/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:3000', // Backend URL
+    },
+  },
+})
