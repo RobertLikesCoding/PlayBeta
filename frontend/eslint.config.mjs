7692f5ad325de039464a107619037e018b5d5710
Robert Baufeld <rbaufeld@posteo.de>
2025-05-21 22:08:57 +0200

    fixed ESLint error

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,13 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import nuxtConfig from './.nuxt/eslint.config.mjs'
 import prettier from 'eslint-config-prettier'
 
-export default withNuxt({
-  extends: [prettier],
-  rules: {
-    'prettier/prettier': 'warn', // shows Prettier issues in ESLint
-    // your other rules here...
+export default [
+  nuxtConfig,
+  ...prettier(),
+  {
+    rules: {
+      'prettier/prettier': 'warn', // shows Prettier issues in ESLint
+      // your other rules here...
+    },
   },
-})
+]
