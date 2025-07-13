ce0e27832938004d4fc17b0faea9cef86de79f85
Robert Baufeld <rbaufeld@posteo.de>
2025-07-13 21:06:18 +0200

    add model relations and validations

--- app/models/game_developer.rb
+++ app/models/game_developer.rb
@@ -1,4 +1,6 @@
 class GameDeveloper < ApplicationRecord
+  has_many :submissions
+
   has_secure_password # validates presence of password, and password_confirmation
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :bio, length: { minimum: 20, maximum: 1000 }, allow_blank: true
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,10 @@
 class Submission < ApplicationRecord
+  belongs_to :game_developer
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :s_id, presence: true, uniqueness: true
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :genre, presence: true, inclusion: { in: %w[Action Adventure RPG Simulation Strategy Sports Puzzle Horror Platformer Shooter Fighting Racing Sandbox] }
+  validates :demo_url, presence: true, format: { with: /\Ahttps:\/\/.+\z/, message: "must start with https://" }
+  validates :version, presence: true, length: { maximum: 20 }
 end
