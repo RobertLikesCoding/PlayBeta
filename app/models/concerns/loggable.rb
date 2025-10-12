92b1bef22e6e7ea693f985ec4c849d38e53c3e69
Robert Baufeld <rbaufeld@posteo.de>
2025-10-12 23:10:43 +0200

    prefer using previous_changes instead of saved_changes

--- app/models/concerns/loggable.rb
+++ app/models/concerns/loggable.rb
@@ -2,36 +2,36 @@ module Loggable
   extend ActiveSupport::Concern
 
   included do
-    after_create :log_create_event
-    after_update :log_update_event
-    after_delete :log_delete_event
+    after_commit :log_create_event, on: :create
+    after_commit :log_update_event, on: :update
+    after_commit :log_delete_event, on: :destroy
   end
 
   private
 
     def log_create_event
-      EventLog.new(
+      EventLog.create(
         loggable: self,
         action: "created",
-        changes_data: saved_changes
+        changes_data: previous_changes
         )
     end
 
     def log_update_event
-      return if saved_changes.blank?
+      return if previous_changes.blank?
 
-      EventLog.new(
+      EventLog.create(
         loggable: self,
         action: "updated",
-        changes_data: saved_changes
+        changes_data: previous_changes.except("updated_at")
         )
     end
 
     def log_delete_event
-      EventLog.new(
+      EventLog.create(
         loggable: self,
         action: "deleted",
-        changes_data: saved_changes
+        changes_data: previous_changes
         )
     end
 end
