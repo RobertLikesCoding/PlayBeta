ec9ad67cdd12f6fda762503540e1467901747c0e
Robert Baufeld <rbaufeld@posteo.de>
2025-11-09 21:16:03 +0100

    add missing model tests for submissions

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -3,11 +3,25 @@ require 'rails_helper'
 RSpec.describe Submission, type: :model do
   subject(:submission) { build(:submission) }
 
-  describe "status" do
-  it "sets default when no status is passed" do
-    expect(submission.status).to eq("in_review")
+  it "is valid with valid attributes" do
+    expect(submission).to be_valid
   end
-end
+
+  it "is invalid with invalid attributes" do
+    submission.title = ""
+    expect(submission).not_to be_valid
+  end
+
+  it "is invalid with an unsafe URL" do
+    submission.demo_url = "http://example.com"
+    expect(submission).not_to be_valid
+  end
+
+  describe "status" do
+    it "sets default when no status is passed" do
+      expect(submission.status).to eq("in_review")
+    end
+  end
 
   describe 'custom validations' do
     it 'adds errors for invalid platforms' do
