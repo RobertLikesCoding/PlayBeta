5b3d1452d2e3c7245745b2d351f14f03406f4b1f
RobertLikesCoding <rbaufeld@posteo.de>
2026-02-20 11:02:48 +0100

    add debug to nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   devServer: { port: 3001 },
-
+  debug: true,
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
