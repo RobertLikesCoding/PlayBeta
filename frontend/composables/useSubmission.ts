d2099fe703d5ef416edb20c686f18534ce689299
RobertLikesCoding <rbaufeld@posteo.de>
2026-04-18 12:49:50 +0200

    set SSR to false and fix page reload issue

--- frontend/composables/useSubmission.ts
+++ frontend/composables/useSubmission.ts
@@ -1,27 +1,30 @@
 import type { GetSubmissionResponse } from '~/types/Submission'
 
-export async function useSubmission(s_id: string) {
+export function useSubmission(s_id: string) {
   const { token } = useAuth()
 
-  const { data, pending, error } = useAsyncData(s_id, async () => {
-    if (!token.value) throw new Error('No auth token')
+  const { data, pending, error } = useAsyncData(
+    `submission-${s_id}`,
+    async () => {
+      if (!token.value) return null
 
-    const response = await $fetch<GetSubmissionResponse>(
-      `/api/v1/submissions/${s_id}`,
-      {
-        baseURL: useRuntimeConfig().public.apiBase,
-        headers: {
-          Authorization: `Bearer ${token.value}`,
+      const response = await $fetch<GetSubmissionResponse>(
+        `/api/v1/submissions/${s_id}`,
+        {
+          baseURL: useRuntimeConfig().public.apiBase,
+          headers: {
+            Authorization: `Bearer ${token.value}`,
+          },
         },
-      },
-    )
+      )
 
-    if ('errors' in response) {
-      console.error(response.errors.join(', '))
-      throw new Error(response.errors.join(', '))
-    }
-    return response.data
-  })
+      if ('errors' in response) {
+        console.error(response.errors.join(', '))
+        throw new Error(response.errors.join(', '))
+      }
+      return response.data
+    },
+  )
 
   return {
     data,
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: { port: 3001 },
   debug: true,
+  ssr: false,
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
