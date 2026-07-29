6a7322fa64b6156a2d443e6baeffb6f9fc1f2d9d
RobertLikesCoding <rbaufeld@posteo.de>
2026-07-30 00:25:53 +0200

    create platform model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
   VALID_GENRES = %w[action adventure rpg simulation strategy sports puzzle horror platformer shooter fighting racing sandbox].freeze
 
   has_and_belongs_to_many :submissions
-  validates :name, inclusion: { in: VALID_GENRES }
+  validates :name, presence: true, uniqueness: true, inclusion: { in: VALID_GENRES }
 end
