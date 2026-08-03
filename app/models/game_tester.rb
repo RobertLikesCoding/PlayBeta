89d33c0e10ab580d02eac697622fe34e9556f447
RobertLikesCoding <rbaufeld@posteo.de>
2026-08-03 21:10:16 +0200

    WIP write tests for game_tester model

--- app/models/game_tester.rb
+++ app/models/game_tester.rb
@@ -3,6 +3,18 @@ class GameTester < ApplicationRecord
   has_many :game_tester_platforms, dependent: :destroy
   has_many :platforms, through: :game_tester_platforms
 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 8 }, on: :create
   enum :gender, { prefer_not_to_say: 0, male: 1, female: 2, non_binary: 3 }
+  validates :gender, presence: true
   validates :age, presence: true, inclusion: { in: 16..100, message: "must be between 16 and 100" }
+
+  def validate_current_password(current_password_params)
+    unless authenticate(current_password_params)
+      errors.add(:current_password, "is incorrect")
+      return false
+    end
+
+    true
+  end
 end
--- spec/models/game_tester_spec.rb
+++ spec/models/game_tester_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe GameTester, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_game_tester) { build(:game_tester, age: 16) }
+
+  context 'is valid' do
+    it 'with a specified gender value' do
+      expect(valid_game_tester).to be_valid
+    end
+    it 'with a specified age of the allowed age range' do
+      expect(valid_game_tester).to be_valid
+    end
+  end
+
+  context 'is invalid' do
+    it 'without a specified gender value' do
+      invalid_game_tester = build(:game_tester, gender: nil)
+      expect(invalid_game_tester).to be_valid
+    end
+    it 'without a specified age' do
+      invalid_game_tester = build(:game_tester, age: nil)
+      expect(invalid_game_tester).to be_invalid
+    end
+    it 'when age is not in allowed range' do
+      invalid_game_tester = build(:game_tester, age: 15)
+      expect(invalid_game_tester).to be_invalid
+    end
+  end
 end
