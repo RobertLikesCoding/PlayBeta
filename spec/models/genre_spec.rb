1bf657f2d7bf40339e31025f1bce081cb31ce8c5
RobertLikesCoding <rbaufeld@posteo.de>
2026-08-23 23:13:06 +0200

    add tests for genre model

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with supported genre name' do
+    subject { build(:genre) }
+    it { is_expected.to be_valid }
+  end
+
+  context 'with duplicate name' do
+    let!(:action_genre) { create(:genre, name: 'action') }
+    subject { build(:genre, name: action_genre.name) }
+    it { is_expected.to be_invalid }
+  end
+  context 'with non supported genre' do
+    subject { build(:genre, name: 'not a genre') }
+    it { is_expected.to be_invalid }
+  end
+  context 'without a name' do
+    subject { build(:genre, name: '') }
+    it { is_expected.to be_invalid }
+  end
 end
