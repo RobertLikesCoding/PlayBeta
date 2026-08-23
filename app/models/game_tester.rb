f43e43c2c8c2b1078c861bf727b0d9ecb6b7d142
RobertLikesCoding <rbaufeld@posteo.de>
2026-08-23 22:49:30 +0200

    write tests for game_tester model

--- app/models/game_tester.rb
+++ app/models/game_tester.rb
@@ -4,7 +4,7 @@ class GameTester < ApplicationRecord
   has_many :platforms, through: :game_tester_platforms
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, length: { minimum: 8 }, on: :create
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
   enum :gender, { prefer_not_to_say: 0, male: 1, female: 2, non_binary: 3 }
   validates :gender, presence: true
   validates :age, presence: true, inclusion: { in: 16..100, message: "must be between 16 and 100" }
